ec7848ae6fc937f0d074bd40c0b45c89b8cf8535
MarcelaEC123 <126629842+MarcelaEC123@users.noreply.github.com>
2024-03-23 18:08:17 -0500

    Ajustes de diseño

--- aplicativo_pos_hc/aplicativo_pos_hc/app.py
+++ aplicativo_pos_hc/aplicativo_pos_hc/app.py
@@ -1,22 +1,18 @@
-# Importamos el módulo redirect
 from flask import Flask, request, render_template, redirect, url_for
 import mysql.connector
 
 app = Flask(__name__, template_folder="C:\\Users\\lenovo\\OneDrive\\Desktop\\UNIAGUSTINIANA\\Proyecto\\Proyecto_web\\aplicativo_pos_hc\\aplicativo_pos_hc\\templates")
 
-# Conexión a la base de datos MySQL
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="SQLMARCE2022.",
-    database="Aplicativo_POS_final"
-)
-
-# Verifica si la conexión a la base de datos fue exitosa
-if db.is_connected():
-    print("Conexión exitosa a la base de datos")
-
-cursor = db.cursor()
+# Función para conectar a la base de datos y crear un cursor
+def conectar_bd():
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="SQLMARCE2022.",
+        database="Aplicativo_POS_final"
+    )
+    cursor = db.cursor()
+    return db, cursor
 
 # Ruta para la página de inicio
 @app.route("/")
@@ -29,18 +25,35 @@ def login():
     username = request.form.get("username")
     password = request.form.get("password")
 
-    # Consulta SQL para verificar las credenciales de inicio de sesión
-    query = "SELECT * FROM usuarios WHERE Usuario = %s AND Contrasenia = %s"
-    cursor.execute(query, (username, password))
-    user = cursor.fetchone()
+    try:
+        # Conectar a la base de datos y crear un cursor
+        db, cursor = conectar_bd()
 
-    # Verifica si se encontró un usuario con las credenciales proporcionadas
-    if user:
-        # Redirigir al usuario a la página principal si el inicio de sesión es exitoso
-        return redirect(url_for("principal"))
-    else:
-        # Si las credenciales son incorrectas, renderiza la plantilla de mensaje de error
-        return render_template("mensaje.html", message="Credenciales incorrectas")
+        # Consulta SQL para verificar las credenciales de inicio de sesión
+        query = "SELECT * FROM usuarios WHERE Usuario = %s AND Contrasenia = %s"
+        cursor.execute(query, (username, password))
+
+        # Leer y procesar los resultados de la consulta
+        user = cursor.fetchone()
+
+        # Cerrar el cursor y la conexión
+        cursor.close()
+        db.close()
+
+        # Procesar los resultados de la consulta
+        if user:
+            # Redirigir al usuario a la página principal si el inicio de sesión es exitoso
+            return redirect(url_for("principal"))
+        else:
+            # Si las credenciales son incorrectas, renderizar la plantilla de mensaje de error
+            error_message = "Credenciales incorrectas"
+            return render_template("mensaje.html", message=error_message)
+
+    except Exception as e:
+        # Manejar cualquier excepción que pueda ocurrir durante la ejecución de la consulta
+        error_message = "Error al procesar la solicitud: {}".format(str(e))
+        return render_template("mensaje.html", message=error_message)
+
 
 # Ruta para la página principal
 @app.route("/principal")
@@ -52,7 +65,39 @@ def principal():
 def logout():
     # Realiza cualquier limpieza necesaria para cerrar sesión
     # Luego redirecciona al usuario a la página de inicio
-    return redirect(url_for("index"))  # Cambiado "home" a "index"
+    return redirect(url_for("index"))  # Aquí cambiamos "home" a "index"
+
+# Rutas para otras páginas
+
+@app.route("/caja")
+def caja():
+    return render_template("caja.html")
+
+@app.route("/clientes")
+def clientes():
+    return render_template("clientes.html")
+
+@app.route("/proveedores")
+def proveedores():
+    return render_template("proveedores.html")
+
+@app.route("/productos")
+def productos():
+    return render_template("productos.html")
+
+@app.route("/compras")
+def compras():
+    return render_template("compras.html")
+
+@app.route("/venta_historico")
+def venta_historico():
+    return render_template("venta_historico.html")
+
+@app.route("/registro")
+def registro():
+    return render_template("registro.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
